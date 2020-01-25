480d18e69164d8c5c5f7ca762bf79e19a35e0f17
Graham McDonald <mcdonag@email.sc.edu>
2020-01-25 15:29:06 -0500

    Update admin.py

--- LLenv/LabLineup/app/admin.py
+++ LLenv/LabLineup/app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from django.contrib.admin.models import LogEntry
+
 from app.models import Lab, Role, Request, LabCode, Notify, Subscription
 
 class LabAdmin(admin.ModelAdmin):
@@ -21,6 +23,9 @@ class NotifyAdmin(admin.ModelAdmin):
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display=('id', 'uid','initialSub','subRenewal','labLimit')
 
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display=("object_id", "content_type","action_flag","user","action_time")
+
 
 admin.site.register(Lab, LabAdmin)
 admin.site.register(Role, RoleAdmin)
@@ -28,3 +33,10 @@ admin.site.register(Request, RequestAdmin)
 admin.site.register(LabCode, LabCodeAdmin)
 admin.site.register(Notify, NotifyAdmin)
 admin.site.register(Subscription, SubscriptionAdmin)
+admin.site.register(LogEntry, LogEntryAdmin)
+
+adminSite = admin.AdminSite
+
+adminSite.site_header = "LabLineup Admin"
+adminSite.site_title = "LabLineup Admin"
+adminSite.index_title = "LabLineup Admin"
